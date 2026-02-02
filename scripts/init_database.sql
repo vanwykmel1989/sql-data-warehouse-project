c8f3df7dd2e706e9d3fbed662e2ff28c7ad8636e
vanwykmel1989 <vanwykmel10@gmail.com>
2026-02-02 13:59:48 +0200

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,15 @@
+-- Create Database 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
