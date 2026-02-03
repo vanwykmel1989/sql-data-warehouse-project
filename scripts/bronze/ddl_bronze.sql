748c3b77b84be1c66da08f65faee4977b578506f
vanwykmel1989 <vanwykmel10@gmail.com>
2026-02-03 12:12:03 +0200

    Add files via upload

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,1 +1,87 @@
+-- CREATE bronze.crm_cust_info
 
+IF  OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.crm_cust_info;
+
+	CREATE TABLE bronze.crm_cust_info(
+		cst_id INT,
+		cst_key NVARCHAR(50),
+		cst_firstname NVARCHAR(100),
+		cst_lastname NVARCHAR(100),
+		cst_marital_status NVARCHAR(50),
+		cst_gender NVARCHAR(50),
+		cst_create_date DATE
+	);
+END
+
+-- CREATE bronze.crm_prd_info
+IF  OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.crm_prd_info;
+
+	CREATE TABLE bronze.crm_prd_info(
+		prd_id INT,
+		prd_key NVARCHAR(50),
+		prd_nm NVARCHAR(50),
+		prd_cost FLOAT,
+		prd_line NVARCHAR(50),
+		prd_start_dt DATE,
+		prd_end_dt DATE
+	);
+END
+
+-- CREATE bronze.crm_sales_details
+IF  OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.crm_sales_details;
+
+	CREATE TABLE bronze.crm_sales_details(
+		sls_ord_num NVARCHAR(50),
+		sls_prd_key NVARCHAR(50),
+		sls_cust_id INT,
+		sls_order_dt INT,
+		sls_ship_dt INT,
+		sls_due_dt INT,
+		sls_sales FLOAT,
+		sls_quantity INT,
+		sls_price FLOAT
+	);
+END
+
+
+-- CREATE bronze.erp_cust_az12
+IF  OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.erp_cust_az12;
+
+	CREATE TABLE bronze.erp_cust_az12(
+		cid NVARCHAR(50),
+		bdate DATE,
+		gen NVARCHAR(50)
+	);
+END
+
+-- CREATE bronze.erp_loc_a101
+IF  OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.erp_loc_a101;
+
+	CREATE TABLE bronze.erp_loc_a101(
+		cid NVARCHAR(50),
+		cntry NVARCHAR(50)
+	);
+END
+
+-- CREATE bronze.erp_px_cat_g1v2
+IF  OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+BEGIN
+	DROP TABLE bronze.erp_px_cat_g1v2;
+
+	CREATE TABLE bronze.erp_px_cat_g1v2(
+		id NVARCHAR(50),
+		cat NVARCHAR(50),
+		subcat NVARCHAR(50),
+		maintenance NVARCHAR(50)
+	);
+END
